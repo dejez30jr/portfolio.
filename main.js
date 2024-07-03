4158734390e53b58c2baec19bb4b54d8caf0d083
dejez30jr <165728707+dejez30jr@users.noreply.github.com>
2024-07-03 15:48:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const menuIcon = document.getElementById("menu-icon")
 const menulist = document.getElementById("menu-list")
+const nav = document.querySelector('nav')
 // const li = document.getElementsByTagName("li")
 menuIcon.addEventListener("click", ()=>{
     menulist.classList.toggle("hidden")
+     nav.style.cssText= "border-radius: 15px 15px 15px 15px;  margin-top: 10px;"
 })
-
 const view = document.getElementById("view")
 view.addEventListener("click", ()=>{
 alert('lanjut ke halaman web')
